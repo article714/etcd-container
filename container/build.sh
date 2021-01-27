250c8848c5ed9ca1964f48a9b1ef0de91501ec36
C. Guychard <christophe@certificare.fr>
2021-01-27 15:31:23 +0100

    [ADD] python3 needed for ansible automation

--- container/build.sh
+++ container/build.sh
@@ -10,7 +10,7 @@ localedef -i fr_FR -c -f UTF-8 -A /usr/share/locale/locale.alias fr_FR.UTF-8
 export LANG=en_US.utf8
 
 # Install basic needed packages
-LC_ALL=C DEBIAN_FRONTEND=noninteractive apt-get install -qy --no-install-recommends wget curl gnupg ca-certificates runit rsyslog logrotate
+LC_ALL=C DEBIAN_FRONTEND=noninteractive apt-get install -qy --no-install-recommends wget curl gnupg ca-certificates runit rsyslog logrotate python3
 
 apt-get upgrade -yq
 
